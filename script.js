e74f624fc99965d5aa881b382788b836d0b4dd27
Branko <mane88@live.de>
2021-09-04 12:49:44 +0200

    working_game_stage1

--- script.js
+++ script.js
@@ -2,73 +2,72 @@
 function computerPlay(){
  let randomComputerNumber = Math.floor(Math.random()*100);
  if (randomComputerNumber <= 33){
-   return("Rock");
+   return("rock");
  }
  else if (randomComputerNumber <= 66){
- return("Paper");
+ return("paper");
 }
  else {
-  return("Scissors")
+  return("scissors")
 }
 }
-console.log(computerPlay())
 
-/**function playerChoose(){
-  let playerInput = prompt("Please choose Rock, Paper or Scissors", "Paper");
+
+function playerChoose(){
+  let playerInput = prompt("Please choose Rock, Paper or Scissors", "Rock").toLowerCase();
   return(playerInput);
-}*/
+}
 
 
 //test the return with console.log()
+const computerSelection = computerPlay();
+  console.log(computerSelection);
+const playerSelection = playerChoose();
+  console.log(playerSelection);
 
 //Write a function that plays a single round of Rock Paper Scissors.
-const playerSelection = "Rock";
-console.log(playerSelection)
-const computerSelection = computerPlay();
-function playRound(computerSelection, playerSelection){
 
-  
+function playRound(){
 
-if (computerSelection === "Rock"){
-  if (playerSelection === "Rock"){
-    return("It's a tie!");
-}
-  else if (playerSelection === "Paper"){
-    return("You won! Paper beats Rock");
-}
-  else if (playerSelection === "Scissors"){
-    return("You lost!Rock beats Scissors");
-}
-  else {
-    return("Sometinh went wrong");
+if (playerSelection === "rock"){
+    if (computerSelection === "paper"){
+        return("You lost!Paper beats Rock");
+    }
+    else if (computerSelection === "scissors"){
+        return("You won!Rock beats Scissors");
+    }
+    else if (computerSelection === "rock"){
+      return("It's a tie")
+    }
+  }
+
+
+if (playerSelection === "paper"){
+  if (computerSelection === "rock"){
+      return("You won!Paper beats Rock");
+  }
+  else if (computerSelection === "scissors"){
+      return("You lost!Scissors beats Paper");
+  }
+  else if (computerSelection === "paper"){
+    return("It's a tie")
   }
 }
 
-else if (computerSelection === "Paper"){
-  if (playerSelection === "Rock"){
-    return("You lost!Paper beats Rock");
-}
-  else if (playerSelection === "Paper"){
-    return("It's a tie!");
-}
-  else if (playerSelection === "Scissors"){
-    return("You won!Scissors beats Paper");
-}
-}
 
-else if (computerSelection === "Scissors"){
-  if (playerSelection === "Rock"){
-    return("You won!Rock beats Scissors");
+if (playerSelection === "scissors"){
+  if (computerSelection === "rock"){
+      return("You lost!Rock beats Scissors");
   }
-  else if (playerSelection === "Paper"){
-    return("You lost!Scissors beats Paper");
+  else if (computerSelection === "paper"){
+      return("You won!Scissors beats Paper");
   }
-  else if (playerSelection === "Scissors"){
-    return("It's a tie!");
+  else if (computerSelection === "scissors"){
+    return("It's a tie")
   }
 }
 }
-console.log(playRound(computerSelection, playerSelection))
+console.log(playRound())
 
 
 
@@ -76,4 +75,5 @@ console.log(playRound(computerSelection, playerSelection))
 //The function should take two parameters - the playerSelection and computerSelection
 //and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
 // Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-//Important note: you want to return the results of this function call, not console.log() them. To test this function console.log the results...
+//Important note: you want to return the results of this function call, not console.log() them. To test this
+//function console.log the results..
