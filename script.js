eaf70ca3ed5812430018e4864cb273b72884e806
Branko <mane88@live.de>
2021-09-22 16:02:36 +0200

    finished and tested

--- script.js
+++ script.js
@@ -4,75 +4,126 @@ let computerScoreCount = 0;
 
 let whoWonTheGame;
 
+
 let scorePl = document.querySelector('.scorePl');
 let scorePc = document.querySelector('.scorePc');
 let winner = document.querySelector('.winner');
-
-
-
+let restartBtn = document.querySelector('.restartBtn');
 const buttons = document.querySelectorAll('button');
+let computerHand = document.querySelector('.pcChoice');
+let playerHands = document.querySelectorAll('.btn');
+const playButtons = document.querySelector('.playButtons');
 
+//player hand animations and interactions
+  document.querySelectorAll('.btn').forEach(btn => {
+    btn.addEventListener('mouseover', () => {
+      computerHand.src = 'images/hand.png';
+      computerHand.style.animation = 'shake 2s ease infinite';
+  });
+  //stop animation after click event and continue after 1.5sec
+  btn.addEventListener('click', function(){
+    computerHand.style.animation = '';
+      setTimeout(function(){
+        computerHand.style.animation = 'shake 2s ease infinite';
+        computerHand.src = 'images/hand.png';
+      }, 700);
+  });
+  //stop animation while not hovering over player hands
+  btn.addEventListener('mouseleave', () => {
+    computerHand.style.animation = 'none';
+  });
+  //clicking feedback through button scale manipulation
+  btn.addEventListener('click', function(event){
+    this.style.animation = 'btnScale 0.2s ease-in-out';
+      setTimeout(function(){
+        event.target.style.animation = '';
+      },200);
+  });
+});
+
+//prevents click-spaming same button while other animations are running
+document.querySelectorAll('.btn').forEach(btn => {
+  btn.addEventListener('click', function(){
+  btn.classList.add('cannotClick');
+    setTimeout(function(){
+      btn.classList.remove('cannotClick')
+    }, 500);
+  });
+});
+
+//click event listeners for player buttons plus checking if someone already won
 buttons.forEach(button => {button.addEventListener('click', function(e){
-  
+    
     if(e.target.id == "rock"){
       playerChoice = e.target.id;
       console.log(playerChoice);
-      playRound(playerChoice, computerPlay());
-      }
+      let computerChoicePic = computerPlay();
+      playRound(playerChoice, computerChoicePic)
+      ;
+      computerHand.src = `images/${computerChoicePic}.png`;
+    }
     else if(e.target.id == "paper"){
       playerChoice = e.target.id;
       console.log(playerChoice);
-      playRound(playerChoice, computerPlay());
+      let computerChoicePic = computerPlay();
+      playRound(playerChoice, computerChoicePic);     
+      computerHand.src = `images/${computerChoicePic}.png`;
     }
     else if(e.target.id == "scissors"){
       playerChoice = e.target.id;
       console.log(playerChoice);
-      playRound(playerChoice, computerPlay());
+      let computerChoicePic = computerPlay();
+      playRound(playerChoice, computerChoicePic);
+      computerHand.src = `images/${computerChoicePic}.png`;
     }
+    
   })
 });
 
-
+//play a round + check if someone won already
 function playRound(humanChoice, computerSelection){
-  
+
   if (humanChoice === "rock"){
      if (computerSelection === "paper"){
         ++computerScoreCount;
         scorePc.innerHTML = computerScoreCount;
-        
+        scoreUpdatePc();
       }
         else if (computerSelection === "scissors"){
           ++playerScoreCount;
           scorePl.innerHTML = playerScoreCount;
-          
+          scoreUpdatePl();
         }
             else if (computerSelection === "rock"){
               ++playerScoreCount;
               ++computerScoreCount;
               scorePl.innerHTML = playerScoreCount;
               scorePc.innerHTML = computerScoreCount;
-              
+              scoreUpdatePl();
+              scoreUpdatePc();
             }
     }
+    
       
       
   if (humanChoice === "paper"){
      if (computerSelection === "rock"){
         ++playerScoreCount;
         scorePl.innerHTML = playerScoreCount;
-        
+        scoreUpdatePl();        
       }
         else if (computerSelection === "scissors"){
           ++computerScoreCount;
           scorePc.innerHTML = computerScoreCount;
-          
+          scoreUpdatePc();
         }
             else if (computerSelection === "paper"){
               ++playerScoreCount;
               ++computerScoreCount;
               scorePl.innerHTML = playerScoreCount;
               scorePc.innerHTML = computerScoreCount;
-             
+              scoreUpdatePl();
+              scoreUpdatePc();
             }
     }
       
@@ -81,19 +132,20 @@ function playRound(humanChoice, computerSelection){
      if (computerSelection === "rock"){
         ++computerScoreCount;
         scorePc.innerHTML = computerScoreCount;
-        
+        scoreUpdatePc();
       }
         else if (computerSelection === "paper"){
           ++playerScoreCount;
           scorePl.innerHTML = playerScoreCount;
-         
+          scoreUpdatePl();          
         }
             else if (computerSelection === "scissors"){
                 ++playerScoreCount;
                 ++computerScoreCount;
                 scorePl.innerHTML = playerScoreCount;
                 scorePc.innerHTML = computerScoreCount;
-                
+                scoreUpdatePl();
+                scoreUpdatePc();
             }
         
     }
@@ -102,7 +154,7 @@ function playRound(humanChoice, computerSelection){
 
 
 
-
+//computer random pick logic
 function computerPlay(){
   let randomComputerNumber = Math.floor(Math.random()*100);
     if (randomComputerNumber <= 33){
@@ -120,143 +172,84 @@ function computerPlay(){
 
 };
 
+//message who won the game + restart button fade in
 function whoWon(){
-  if (playerScoreCount == 5){
+  if ((playerScoreCount === 5) && (computerScoreCount === 5)){
+    whoWonTheGame = 'Ohh...It\'s a tie!'
+    winner.textContent = whoWonTheGame;
+    showWinner();
+    showButton();
+    hideInputs();
+  }
+  else if (playerScoreCount == 5){
     whoWonTheGame =  'Player Won';
     winner.textContent = whoWonTheGame;
+    showWinner();
+    showButton();
+    hideInputs();
   }
   else if (computerScoreCount == 5){
     whoWonTheGame = 'Computer Won';
     winner.textContent = whoWonTheGame;
+    showWinner();
+    showButton();
+    hideInputs();
   }
-}
+};
 
-/*
-function whoWon(){
-  if((playerScoreCount == 5) || (computerScoreCount == 5)){
-    if(playerScoreCount>computerScoreCount){
-      alert('Player won!');
-    }
-    else if(playerScoreCount<computerScoreCount){
-      alert('Computer won!');
-    }
+//hide player input options after a game concludes
+function hideInputs(){
+  playButtons.classList.add('hideInputs');
+  };
+
+//stops countig points after someone has 5 !!!NOT WORKING!!!
+function stopGame(){
+  if(playerScoreCount == 5 || computerScoreCount == 5){
+    return;
   }
-}*/
+};
 
-/*
-let playerScoreCount = 0;
+//game restart button fade in
+function showButton(){
+  restartBtn.classList.add('showBtn');
+};
 
-let computerScoreCount = 0;
+//winner message fade in
+function showWinner(){
+  winner.classList.add('showWinner');
+};
 
-//Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
-function game(){
+//restart game function attached to button click
+document.getElementById("restart").addEventListener("click", function() {
+  location.reload();
+});
 
-//computers choice for ‘Rock’, ‘Paper’ or ‘Scissors’
-function computerPlay(){
-  let randomComputerNumber = Math.floor(Math.random()*100);
-  if (randomComputerNumber <= 33){
-    return("rock");
+//color and scale indication for who got a point after a round and who won
+function scoreUpdatePl(){
+  if(playerScoreCount == 5){
+    scorePl.style.transform = 'scale(1.5)';
+    scorePl.style.color = 'red';
+    return;
   }
-  else if (randomComputerNumber <= 66){
-  return("paper");
- }
-  else {
-   return("scissors")
-   }
-}
- 
- 
-//player input that is case-insensitive
-function playerChoose(){
-  let playerInput = prompt("Please choose Rock, Paper or Scissors", "Rock").toLowerCase();
-  return(playerInput);
-}
- 
- 
-//test the return with console.log()
- const computerSelection = computerPlay();
-   console.log(computerSelection);
- const playerSelection = playerChoose();
-   console.log(playerSelection);
- 
-
-//compare inputs and return outcome 
-function playRound(){
-
-if (playerSelection === "rock"){
-   if (computerSelection === "paper"){
-      return("You lost!Paper beats Rock");
-    }
-      else if (computerSelection === "scissors"){
-          return("You won!Rock beats Scissors");
-      }
-          else if (computerSelection === "rock"){
-           return("It's a tie")
-          }
+  scorePl.style.transform = 'scale(1.5)';
+  scorePl.style.color = 'red';
+  setTimeout(function(){
+    scorePl.style.transform = 'scale(1)';
+    scorePl.style.color = 'rgba(255, 255, 255, 0.89)';
+  },700);
+};
+//color and scale indication for who got a point after a round and who won
+function scoreUpdatePc(){
+  if(computerScoreCount == 5){
+    scorePc.style.transform = 'scale(1.5)';
+    scorePc.style.color = 'red';
+    return;
   }
-    
-    
-if (playerSelection === "paper"){
-   if (computerSelection === "rock"){
-      return("You won!Paper beats Rock");
-    }
-      else if (computerSelection === "scissors"){
-        return("You lost!Scissors beats Paper");
-      }
-          else if (computerSelection === "paper"){
-           return("It's a tie")
-          }
-  }
-    
-    
-if (playerSelection === "scissors"){
-   if (computerSelection === "rock"){
-      return("You lost!Rock beats Scissors");
-    }
-      else if (computerSelection === "paper"){
-         return("You won!Scissors beats Paper");
-      }
-          else if (computerSelection === "scissors"){
-           return("It's a tie")
-          }
-  }
-  
-}
-
-
-
-//point awarding system 
-if (playRound() === "It's a tie"){
-    console.log("It's a tie");
-    return(++playerScoreCount, ++computerScoreCount);
-}
-  else if (playRound().substring(4, 5) === "w"){
-    console.log("You win");
-    return(++playerScoreCount);
-  }
-    else if (playRound().substring(4, 5) === "l"){
-      console.log("You loose");
-      return(++computerScoreCount);
-  }
-  
-}
-
-// 5 game loop with score after each iterration
-for(i = 0; i < 5; i++){
-game();
-console.log("Player",playerScoreCount);
-console.log("Computer", computerScoreCount);
-}
-
-//message for the match outcome
-if(playerScoreCount === computerScoreCount){
-  console.log("This match resultet in a tie.");
-  }
-   else if(playerScoreCount > computerScoreCount){
-    console.log("Humans rule!");
-  }
-     else if(playerScoreCount < computerScoreCount){
-     console.log("Skynet is coming!");
-}
-*/
+  scorePc.style.transform = 'scale(1.5)';
+  scorePc.style.color = 'red';
+  setTimeout(function(){
+    scorePc.style.transform = 'scale(1)';
+    scorePc.style.color = 'rgba(255, 255, 255, 0.89)';
+  },700);
+};
 
